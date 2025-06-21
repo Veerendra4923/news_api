769780f07abc7be163177ddf763c8c6e05a19581
Mogilicharla Veerendra Sai <mogilicharlaveerendrasai@gmail.com>
2025-06-21 17:26:51 +0530

    add links,specify lang,limit new,add subject,control category

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import requests
 from send_mail import send_email
-
+topic="tesla"
 api_key = 'ab6983f6da7d4946a9eb2d1e6312b8fa'
-url="https://newsapi.org/v2/everything?q=tesla&from=2025-05-21&sortBy=publishedAt&apiKey=ab6983f6da7d4946a9eb2d1e6312b8fa"
+url=("https://newsapi.org/v2/everything?"
+     f"q={topic}&"
+     "from=2025-05-21&"
+     "sortBy=publishedAt&"
+     "apiKey=ab6983f6da7d4946a9eb2d1e6312b8fa&"
+     "language=en")
 response = requests.get(url)
 content=response.json()
 body=""
-for article in content['articles']:
-    body=body+article['title']+"\n"+article['description']+"\n"+2*"\n"
+for article in content['articles'][:20]:
+    if article["title"] is not None:
+        body=("Subject: Todays's news"+
+              "\n"+body+article['title']+"\n"
+              +article['description']
+              +"\n"+article['url']+"\n"+2*"\n")
 
 body=body.encode('utf-8')
 send_email(body)
